0ea92b61b053ae31aee3421cd6689e3bd20bc227
wcgordon1 <wcgordon1@gmail.com>
2021-11-15 14:44:40 -0800

    first commit, copied codepen JS for THe Odin Project Rock paper Scissors UI

---
+++ app.js
@@ -0,0 +1,60 @@
+const getUserChoice = userInput => {
+    useerInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'scissors' || userInput === 'paper') {
+        return userInput;
+    } else {
+        console.log('Error, please type: rock, paper, or scissors.');
+    }
+}
+
+const getComputerChoice = () => {
+    const randomNumber =
+        Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+};
+
+const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+        return 'This game is a tie!';
+    }
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'Sorry, computer won!';
+        } else {
+            return 'Congrats, you won!';
+        }
+    }
+
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'Sorry, computer won!';
+        } else {
+            return 'Congrats, you won!';
+        }
+    }
+
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'Sorry, computer won!';
+        } else {
+            return 'Congrats, you won!';
+        }
+    }
+};
+
+const playGame = () => {
+    const userChoice = getUserChoice('paper');
+    const computerChoice = getComputerChoice();
+    console.log('You threw: ' + userChoice);
+    console.log('The computer threw: ' + computerChoice);
+    console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
